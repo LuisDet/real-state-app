ed4719e24f901b781a634651ca494ee2c01cf3d4
LuisDet <luisedenist@gmail.com>
2022-01-25 13:22:12 -0400

    Loader spinner

--- components/project-list/index.js
+++ components/project-list/index.js
@@ -1,10 +1,30 @@
 import ProjectCard from '../project-card'
 import { useDataFetch } from '../../hooks/useDataFetch'
+import { TailSpin } from 'react-loader-spinner'
 
 export default function ProjectList() {
   const { data: projects } = useDataFetch('project')
-  console.log(projects)
-  if (!projects) return <div>loading...</div>
+  if (!projects)
+    return (
+      <div className='loader-container'>
+        <TailSpin
+          ariaLabel='loading-indicator'
+          color={'#06A0A2'}
+          height={150}
+          width={150}
+        />
+        <style jsx>{`
+          .loader-container {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            width: 100%;
+            height: 70vh;
+            background-color: #00000011;
+          }
+        `}</style>
+      </div>
+    )
   if (!projects.length) return <div>Not Found</div>
   return (
     <>
